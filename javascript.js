e8d1e3a94c4c0b6f7df3e2045e1a50e0be0cc009
ChangAndyChang <77998897+ChangAndyChang@users.noreply.github.com>
2026-09-10 23:18:45 -0700

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,1 +1,14 @@
 
+$( document ).ready(function() {
+	// Handler for .ready() called.
+	$( "#it_toggle" ).on("click", function() {
+		$(this).toggleClass("clicked");
+	});
+
+	$('.has_dropdown').hover(function() {
+	    $(this).toggleClass('active');
+	});
+
+
+
+});
